e6b3c3974ddbf4fad2ecf9ae59ca43bc96eef9e4
Ephemeral Astronaut <troglodytto@gmail.com>
2022-09-07 18:01:13 +0530

    Update Aliases

--- .config/fish/conf.d/02alias.fish
+++ .config/fish/conf.d/02alias.fish
@@ -2,13 +2,13 @@ abbr --add cpoy 'xclip -selection c'
 abbr --add pasta 'xclip -selection c -o'
 abbr --add python 'python3'
 abbr --add pip 'pip3'
-abbr --add mysql 'docker run -it --network bridge --rm mysql mysql -h 172.17.0.1 -uroot -p'
-abbr --add psql 'docker run -it --rm --network bridge postgres psql -h 172.17.0.1 -U postgres -W'
-abbr --add redis 'docker run -ti --network bridge --rm redis redis-cli -h 172.17.0.1'
+abbr --add mysql 'mysql -h localhost -uroot -p'
+abbr --add psql 'psql -h localhost -U postgres -W'
+abbr --add redis 'redis-cli -h localhost'
 abbr --add vim 'lvim'
 abbr --add vi 'nvim'
 abbr --add ls 'exa -l --icons'
-abbr --add cat 'bat'
+abbr --add cat 'batcat'
 abbr --add kilmux "tmux list-sessions | grep -v attached | awk 'BEGIN{FS=\":\"}{print $1}' | xargs -n 1 tmux kill-session -t"
 abbr --add grep 'rg'
 abbr --add du 'dust -d 1'
@@ -16,9 +16,16 @@ abbr --add tmux 'tmux -u'
 abbr --add avd 'avdmanager'
 abbr --add tree 'exa --tree --icons --level=4'
 
-function office --wraps git --description "Setup the current repository for office use"
-  git config --local user.name "Piyush Upadhyay"
+function office --wraps git --description "Setup the current repository for Rivi use"
+  git config --local user.name "Piyush Devara"
   git config --local user.email piyush@rivi.co
-  git config --local user.signingkey DE042B1B56FBD106278C17D9927F7DB60649C8DE
+  git config --local user.signingkey (gpg --list-keys piyush@rivi.co | sed -n 2p | xargs)
+  git config --local commit.gpgsign true
+end
+
+function decathlon --wraps git --description "Setup the current repo for Decathlon use"
+  git config --local user.name "Piyush Devara"
+  git config --local user.email piyush.devara.partner@decathlon.com
+  git config --local user.signingkey (gpg --list-keys piyush.devara.partner@decathlon.com | sed -n 2p | xargs)
   git config --local commit.gpgsign true
 end
